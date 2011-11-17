f88c69e47b859343bb6c440b92ca7fdbd5c6d95e
Alberto Correa <alberto.correa@gmail.com>
2011-11-17 18:39:51 -0500

    Initial commit, new rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Miiglesiaenlinea::Application.config.secret_token = 'c54b00e6a392183062a6209c8a5a1d0a64546dd324b2fa3747b76b35dc4d0a16d5d7513ada461c61d2d72eaabf7a69384a90358c8a65028aa88c3750dbbcc16e'
